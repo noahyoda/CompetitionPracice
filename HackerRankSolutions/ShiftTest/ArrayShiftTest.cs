da2870df56682b21df4c54695a3adf12af56c96a
BuildTools <noahyoda1@gmail.com>
2021-10-08 18:55:59 -0600

    started tests for array shift solution

---
+++ HackerRankSolutions/ShiftTest/ArrayShiftTest.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShiftTest
+{
+    [TestClass]
+    public class ArrayShiftTest
+    {
+        [TestMethod]
+        public void TestSimpleShift()
+        {
+            int[] a = { 1, 2, 3, 4 };
+            List<int> test = a.ToList();
+
+            List<int> result = ArrayShift.ArrayShift.ShiftLeft(test, 2);
+
+            int[] e = { 3, 4, 1, 2 };
+            List<int> expected = e.ToList();
+
+            CollectionAssert.AreEqual(expected, result);
+        }
+    }
+}
