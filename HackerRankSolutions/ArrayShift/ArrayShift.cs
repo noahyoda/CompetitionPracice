4baf7ca03a0daa36331604df23841786625dfd91
BuildTools <noahyoda1@gmail.com>
2021-10-08 18:55:48 -0600

    added solution to another hackerrank challenge

---
+++ HackerRankSolutions/ArrayShift/ArrayShift.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArrayShift
+{
+    public class ArrayShift
+    {
+
+        public static List<int> ShiftLeft(List<int> arr, int p)
+        {
+            List<int> result = new List<int>();
+            for (int i = 0; i < arr.Count; i++)
+            {
+                int pos = i + p;
+                if (pos >= arr.Count)
+                    pos = pos - arr.Count;
+                result.Add(arr[pos]);
+            }
+
+            return result;
+        }
+        
+    }
+}
